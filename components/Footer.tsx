73509b9e52a8d1099bb8872dc141e1314fe7c0af
Tamasce <giovannistancampiano1999@gmail.com>
2024-09-21 15:26:31 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           Ready to take {' '} <span className="text-purple">your </span> digital presence to the next level?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you achieve your goals
+        Reach out to me today, and let&apos;s discuss how I can help your company achieve its goals.
         </p>
         <a href="mailto:giovannistancampiano1999@gmail.com">
           <MagicButton title="Let's get in touch" icon={<FaLocationArrow />} position="right"/>
@@ -18,15 +18,22 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-        Copyright © 2024 Giovanni
+        Copyright © 2024 Giovanni Stancampiano
         </p>
         <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((profile)=>(
-            <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-              <img src={profile.img} key={profile.id} width={20} height={20}/>
-            </div>
+          {socialMedia.map((profile) => (
+            <a
+              key={profile.id}
+              href={profile.url}  // Assumi che "profile.href" contenga il link
+              target="_blank"      // Apre il link in una nuova scheda
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={profile.img} key={profile.id} width={20} height={20} />
+            </a>
           ))}
         </div>
+
       </div>
     </footer>
   )

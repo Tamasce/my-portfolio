02fd0400d37fde583f7b6284827f6cce8aff5e99
Tamasce <giovannistancampiano1999@gmail.com>
2024-09-21 15:16:49 +0200

    Update 3d-pin.tsx

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 
 export const PinContainer = ({
   children,
@@ -28,14 +28,22 @@ export const PinContainer = ({
     setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
   };
 
+  // Funzione per gestire il click e redirigere al link
+  const handleClick = () => {
+    if (href) {
+      window.location.href = href;
+    }
+  };
+
   return (
     <div
       className={cn(
-        "relative group/pin z-50  cursor-pointer",
+        "relative group/pin z-50 cursor-pointer",
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      onClick={handleClick}  // Aggiungi onClick per redirigere al link
     >
       <div
         style={{
@@ -48,16 +56,16 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
-          // remove  bg-black
           className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
         >
-          <div className={cn(" relative z-50 ", className)}>{children}</div>
+          <div className={cn("relative z-50", className)}>{children}</div>
         </div>
       </div>
       <PinPerspective title={title} href={href} />
     </div>
   );
 };
+
 
 export const PinPerspective = ({
   title,
@@ -76,7 +84,7 @@ export const PinPerspective = ({
             target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
           >
-            <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
+            <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5 text-nowrap">
               {title}
             </span>
 
